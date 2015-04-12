66cde9a2fc47d2b2c300ea4ecd99e99e70960a9f
marina-bard <marina.bard.96@gmail.com>
2015-04-12 23:17:00 +0300

    GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->mainToolBar->hide();
+    ui->tabMenu->setCurrentIndex(0);
+    //ui->tableWidget->setShowGrid(true);
+   // for(int i = 0; i < 6; i++)
+     //   ui->tableWidget->setColumnHidden(i, false);
 }
 
 MainWindow::~MainWindow()
